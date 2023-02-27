7268493aab3489968e0144e4b7218ec704bf1784
Arden <arden.haldorson@gmail.com>
2023-02-27 15:13:31 +0100

    testing changing gridarea with click-event

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,3 +3,16 @@ import { io, Socket } from 'socket.io-client'
 import { ClientToServerEvents, ServerToClientEvents } from '@backend/types/shared/SocketTypes'
 
 const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
+
+const boardEl = document.querySelector('#board') as HTMLDivElement
+const testingEl = document.querySelector('#testing') as HTMLDivElement
+
+const var1 = 3
+const var2 = 5
+
+boardEl.addEventListener('click', () => {
+	testingEl.classList.add('black')
+	// testingEl.style.gridArea = "4 / 5 / 5 / 6"
+	testingEl.style.gridArea = `${var1} / ${var2} / ${var1 + 1 } / ${var2 + 1}`
+
+})
